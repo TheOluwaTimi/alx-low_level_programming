afe68e7dc52773c32d7f279505c1c799521e64d6
Theoluwatimi <theoluwatimi@gmail.com>
2022-09-15 14:54:02 -0700

    task o c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,29 +1,23 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
- * main - print if the nunber is positive, negative or zero.
- * Description: using the main function.
- * this program prints "prigramming is positive, negative or zero"
- * Return 0
+ * main - Entry point
+ *
+ * Return: Always success
  */
 int main (void)
 {
 	int n;
-	srand (time(0))
-		n = rand() - RAND_MAX / 2
-		if (n > 0)
-{
-	printf("%i is positive\n", n);
-}
-else if (n == 0);
-{
-printf("%i is zero\n", n;
-		}
-		else if ( n < 0)
-		printf("%i is negative\n".)
-		}
-		return (0)
-		}
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n
+				else
+				printf("%d is negaative\n", n);
+
+				return (0);
+				}
