a043cee087e8b62797096afc3a5a4e116c12f349
Theoluwatimi <theoluwatimi@gmail.com>
2022-09-15 10:48:34 -0700

    positive and negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print if the nunber is positive, negative or zero.
+ * Description: using the main function.
+ * this program prints "prigramming is positive, negative or zero"
+ * Return 0
+ */
+int main (void)
+{
+	int n;
+	srand (time(0))
+		n = rand() - RAND_MAX / 2
+		if (n > 0)
+{
+	printf("%i is positive\n", n);
+}
+else if (n == 0);
+{
+printf("%i is zero\n", n;
+		}
+		else if ( n < 0)
+		printf("%i is negative\n".)
+		}
+		return (0)
+		}
+
