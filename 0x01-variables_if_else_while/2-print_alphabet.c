340b3174bd4e05fa92ff5f41d3d933b74cd182b7
Theoluwatimi <theoluwatimi@gmail.com>
2022-09-19 05:45:55 -0700

    print 2 alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * *main - Entry point
- * *
- * * Return; Always success
- * */
+ *main - Entry point
+ *
+ * Return: Always success
+ */
 int main (void)
 {
-	    char zh = 'a';
-	        while (zh <= 'z')
-			    {
-				            putchar(zh);
-					            ++zh;
-						        }
-		    putchar('\n');
-		        return(0);
+    char zh = 'a';
+    while (zh <= 'z')
+    {
+	    putchar(zh);
+	    ++zhh
+    }
+    putchar('\n');
+    return(0);
 }
