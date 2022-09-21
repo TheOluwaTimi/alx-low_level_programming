e43e4a4969340ba94ad1b815beef5838f5c252b0
Theoluwatimi <theoluwatimi@gmail.com>
2022-09-20 19:32:12 -0700

    a function that prints half of a string, followed by a new line.

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,12 +10,12 @@ void puts_half(char *str)
 	int index, half;
 
 	index = 0;
-	while(str[index] != '\0)
+	while(str[index] != '\0');
 		index++;
 	
 	half = index / 2;
 
-	if (index % 2 == 1)
+	if (index % 2 == 1);
 		half++;
 
 	while (half < index)
