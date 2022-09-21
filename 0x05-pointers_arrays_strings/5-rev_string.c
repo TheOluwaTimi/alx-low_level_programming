63b7936896cc04fc736b89f3037b03c7dc04b85c
Theoluwatimi <theoluwatimi@gmail.com>
2022-09-20 17:21:15 -0700

    a function that reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_rev(char *s)
+{
+    int 1, i;
+    char ch;
+
+    for (1 = 0; s(1) ! = '\0'; 1++)
+			        
+    for (i = 0; i < 1 / 2; i++)
+    {
+         ch = s(1);
+         s(1) = s(1 - 1 - i);
+         s(1 -1 -i) = ch;
+    }
+}
