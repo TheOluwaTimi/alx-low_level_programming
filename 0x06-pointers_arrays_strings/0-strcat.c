c2e81fb29c754f3483f94e69aa5811ea415293e4
Theoluwatimi <theoluwatimi@gmail.com>
2022-09-22 04:15:21 -0700

    function that concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,8 +4,6 @@
  *  an inputted number of bytes from src
  * @dest: The string to be appended upon.
  * @src: The string to be appended to dest.
- * @n: The number of bytes from src to be appended to dest.
- *
  * Return: A pointer to the resulting string dest.
  */
 char *_strcat(char *dest, char *src)
