01de671aa9e43f6fa678f138ad15e02af12db93d
Theoluwatimi <theoluwatimi@gmail.com>
2022-09-21 21:00:00 -0700

    task 1 strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,23 @@
 /**                                                                                                                                  
- *  * _strncat - Concatenates two strings using at most                                                                                 
- *   *            an inputted number of bytes from src.                                                                                  
- *    * @dest: The string to be appended upon.                                                                                            
- *     * @src: The string to be appended to dest.                                                                                          
- *      * @n: The number of bytes from src to be appended to dest.                                                                          
- *       *                                                                                                                                   
- *        * Return: A pointer to the resulting string dest.                                                                                   
- *         */                                                                                                                                  
+ * _strncat - Concatenates two strings using at most                                                                                 
+ *  an inputted number of bytes from src.                                                                                  
+ * @dest: The string to be appended upon.                                                                                            
+ * @src: The string to be appended to dest.                                                                                          
+ * @n: The number of bytes from src to be appended to dest.                                                                          
+ *                                                                                                                                   
+ * Return: A pointer to the resulting string dest.                                                                                   
+ */                                                                                                                                  
 char *_strncat(char *dest, char *src, int n)                                                                                         
-{                                                                                                                                    
-	        int index = 0, dest_len = 0;                                                                                                 
-		                                                                                                                                     
-		        while (dest[index++])                                                                                                        
-				                dest_len++;                                                                                                          
-			                                                                                                                                     
-			        for (index = 0; src[index] && index < n; index++)                                                                            
-					                dest[dest_len++] = src[index];                                                                                       
-				                                                                                                                                     
-				        return (dest);                                                                                                               
-}
+{
+	int len = 0. i;
 
+	while (dest[len])
+		len++;
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[len] = src[i];
+		lem += 1;
+	}
+	dest[len] = '\0';
+	return (dest)
+}
