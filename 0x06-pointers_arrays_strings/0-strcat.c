c4dfde36750e32a7cc7fc52745774d96a2e34532
Theoluwatimi <theoluwatimi@gmail.com>
2022-09-21 21:07:54 -0700

    task 1 strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,13 +1,14 @@
+#include "main.h"
 /**                                                                                                                                  
- * _strncat - Concatenates two strings using at most                                                                                 
- *  an inputted number of bytes from src.                                                                                  
- * @dest: The string to be appended upon.                                                                                            
- * @src: The string to be appended to dest.                                                                                          
- * @n: The number of bytes from src to be appended to dest.                                                                          
- *                                                                                                                                   
- * Return: A pointer to the resulting string dest.                                                                                   
+ * _strncat - Concatenates two strings using at most
+ *  an inputted number of bytes from src
+ * @dest: The string to be appended upon.
+ * @src: The string to be appended to dest.
+ * @n: The number of bytes from src to be appended to dest.
+ *
+ * Return: A pointer to the resulting string dest.
  */                                                                                                                                  
-char *_strncat(char *dest, char *src, int n)                                                                                         
+char *_strncat(char *dest, char *src, int n)
 {
 	int len = 0. i;
 
@@ -19,5 +20,5 @@ char *_strncat(char *dest, char *src, int n)
 		lem += 1;
 	}
 	dest[len] = '\0';
-	return (dest)
+	return (dest);
 }
