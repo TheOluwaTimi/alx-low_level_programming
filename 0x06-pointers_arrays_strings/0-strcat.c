81e8f380cba47637d8181fc290a3439beae333c2
Theoluwatimi <theoluwatimi@gmail.com>
2022-09-21 19:43:38 -0700

    a function that concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcat - concetrates the string pointed to by src to the end of the string pointed by dst.
+ * string of dst will be appended 
+ * src strings will be concetrated upon
+ *
+ * Return: returns pioner to dest
+ */
+
+char*_strcat(char*dest, char*src)
+{
+	int index = 0, dest_den = 0;
+
+	while (dest[index ++1)
+		dest_len++;
+
+	for (index = 0; src[index]; index ++)
+		dest[dest_len++] = src [index];
+
+	return (dest);
+
+}
