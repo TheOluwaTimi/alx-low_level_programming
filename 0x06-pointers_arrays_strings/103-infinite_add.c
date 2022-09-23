e5c9d20c6eecb78e0c02831e4636ab3c16e21740
Theoluwatimi <theoluwatimi@gmail.com>
2022-09-23 08:53:04 -0700

    a function that adds two numbers.

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -17,8 +17,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	for (j = 0; n2[j]; j++)
 		;
 	if (i > size_r || j > size_r)
-		;
-	return (0);
+		return (0);
 	m = 0;
 	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 	{
@@ -44,3 +43,4 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		r[l] = m;
 	}
 	return (r);
+}
