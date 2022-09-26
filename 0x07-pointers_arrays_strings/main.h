74d298d1629c80fd2a05f5d5752d71883fe69c5e
Theoluwatimi <theoluwatimi@gmail.com>
2022-09-26 06:08:45 -0700

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,7 +7,6 @@
  * Description: Header file containing all prototypes for 0x07 arrays and strings
  */
 
-#include <stddef.h>
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
