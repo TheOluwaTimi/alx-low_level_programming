2e3109e78fc7b31587eafb496e31a172887e5b69
Theoluwatimi <theoluwatimi@gmail.com>
2022-09-26 08:12:59 -0700

    a function that fills memory with a constant byte.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,14 +2,15 @@
 #define MAIN_H
 
 /**
- * file:main.h
- * Author: Adejumoi Timilehin
- * Description: Header file containing all prototypes for 0x07 arrays and strings
+ * file: main.h
+ * Auth: Oluwatimi
+ * Description: Header file containing prototypes for all functions
+ * used in arrays and strings 
  */
 
-int _putchar(char c);
+int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *int _putchar(char c);dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
@@ -17,5 +18,5 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-int _putchar(char c);
-#endif
+
+#endif /*MAIN_H*/
