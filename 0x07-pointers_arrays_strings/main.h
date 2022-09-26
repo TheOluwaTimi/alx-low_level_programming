dc1f91c4c38a1d05861e2e9802dbf7ca561b3c60
Theoluwatimi <theoluwatimi@gmail.com>
2022-09-26 08:36:31 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,11 +5,11 @@
  * file: main.h
  * Auth: Oluwatimi
  * Description: Header file containing prototypes for all functions
- * used in arrays and strings 
+ * used in arrays and strings
  */
 
-int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
