e497082f36f731a9f6cd73d9483a1984460d27fc
Theoluwatimi <theoluwatimi@gmail.com>
2022-09-27 12:58:55 -0700

    all

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,22 +1,21 @@
 #include "main.h"
-#include "stdio.h"
-#include <stddef>
 
 /**
  * memset - fill a block of memory
  * @s: starting address
  * @b: value
  * @n: no of value
+ *
  * Return: Always
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
+	unsigned int i = 0;
 
-	for (i = n; i > 0; (i++)
+	while (i < n)
 	{
 		s[i] = b;
-		n--;
+		i++;
 	}
 	return (s);
 }
